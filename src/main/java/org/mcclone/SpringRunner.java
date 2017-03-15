f1da8f8a3997eaec84c98f8b6cc51914297e85e4
mcclone <abc15606@163.com>
2017-03-15 21:50:05 +0800

    CountDownLatch

--- src/main/java/org/mcclone/SpringRunner.java
+++ src/main/java/org/mcclone/SpringRunner.java
@@ -3,6 +3,10 @@ package org.mcclone;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * @author McClone
  */
@@ -10,5 +14,28 @@ public class SpringRunner {
 
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"spring.xml"});
+        CountDownLatch countDownLatch = new CountDownLatch(5);
+        ExecutorService executorService = Executors.newFixedThreadPool(6);
+        executorService.submit(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("123");
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        System.out.println("Hello World");
+        for (int i = 0; i < 5; i++) {
+            new Thread(new Job(countDownLatch)).start();
+        }
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("end");
     }
 }
