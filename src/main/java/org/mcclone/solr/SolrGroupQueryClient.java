1abd76d2fa2da4f4e9d1bc26811f80d8bfb10104
mcclone <abc15606@163.com>
2017-06-08 22:25:08 +0800

    solr统计

--- src/main/java/org/mcclone/solr/SolrGroupQueryClient.java
+++ src/main/java/org/mcclone/solr/SolrGroupQueryClient.java
@@ -5,14 +5,9 @@ import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.impl.HttpSolrClient;
 import org.apache.solr.client.solrj.response.FacetField;
-import org.apache.solr.client.solrj.response.FieldStatsInfo;
 import org.apache.solr.client.solrj.response.QueryResponse;
-import org.apache.solr.common.SolrDocumentList;
-import org.apache.solr.common.params.StatsParams;
 
 import java.io.IOException;
-import java.util.List;
-import java.util.Map;
 
 /**
  * solr统计
@@ -24,22 +19,12 @@ public class SolrGroupQueryClient {
         String urlString = "http://localhost:8983/solr/test";
         SolrClient solr = new HttpSolrClient.Builder(urlString).build();
         SolrQuery solrParams = new SolrQuery("*:*");
-        solrParams.setParam(StatsParams.STATS_FACET, "name");
-        solrParams.setGetFieldStatistics("flow");
-        solrParams.setGetFieldStatistics(true);
+        solrParams.setFacet(true);
+        solrParams.addFacetField("name");
         QueryResponse response = solr.query(solrParams);
-        Map<String, FieldStatsInfo> map = response.getFieldStatsInfo();
-        for (String key : map.keySet()) {
-            FieldStatsInfo f = map.get(key);
-            System.out.println(f.getFacets());
-            System.out.println(f.getName());
-            System.out.println(f.getCount());
-            System.out.println(f.getMax());
-            System.out.println(f.getMin());
-            System.out.println(f.getSum());
-            System.out.println(f.getMean());
-            System.out.println(f.getMissing());
-            System.out.println(f.getStddev());//标准差
-        }
+        FacetField facetField = response.getFacetField("name");
+        facetField.getValues().forEach(count -> {
+            System.out.println(count.getName() + ":" + count.getCount());
+        });
     }
 }
