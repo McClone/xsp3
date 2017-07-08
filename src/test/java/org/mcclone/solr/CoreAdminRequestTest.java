971870d483458d22e693136a81d34c6446bace60
mcclone <abc15606@163.com>
2017-07-08 12:28:56 +0800

    solr core

---
+++ src/test/java/org/mcclone/solr/CoreAdminRequestTest.java
@@ -0,0 +1,36 @@
+package org.mcclone.solr;
+
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.impl.HttpSolrClient;
+import org.apache.solr.client.solrj.request.CoreAdminRequest;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+
+/**
+ * Created by mcclone on 17-7-8.
+ */
+public class CoreAdminRequestTest {
+    private HttpSolrClient solrClient;
+
+    @Before
+    public void setUp() throws Exception {
+        solrClient = new HttpSolrClient.Builder("http://localhost:8983/solr").build();
+    }
+
+
+    @Test
+    public void renameCore() throws Exception {
+        try {
+            CoreAdminRequest.renameCore("test", "test_new", solrClient);
+        } catch (SolrServerException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void createCore() throws Exception {
+        CoreAdminRequest.createCore("test_1", "test_1", solrClient,"solrconfig.xml","schema.xml");
+    }
+}
