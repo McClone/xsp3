aceb91402198bec978faa67039c53ced78ca045a
mcclone <abc15606@163.com>
2017-07-14 23:06:14 +0800

    solr

---
+++ src/test/java/org/mcclone/solr/LBHttpSolrClientTest.java
@@ -0,0 +1,37 @@
+package org.mcclone.solr;
+
+import org.apache.commons.lang.math.RandomUtils;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.impl.LBHttpSolrClient;
+import org.apache.solr.client.solrj.request.UpdateRequest;
+import org.apache.solr.common.SolrInputDocument;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.UUID;
+
+/**
+ * Created by mcclone on 17-7-14.
+ */
+public class LBHttpSolrClientTest {
+
+    @Test
+    public void test() throws IOException, SolrServerException {
+
+        String[] servers = {"http://localhost:9031/solr/", "http://localhost:9032/solr/"};
+        LBHttpSolrClient lbHttpSolrClient = new LBHttpSolrClient.Builder().withBaseSolrUrls(servers).build();
+
+        for (int i = 0; i < 10; i++) {
+            SolrInputDocument document = new SolrInputDocument();
+            document.addField("id", UUID.randomUUID().toString().replaceAll("-", ""));
+            document.addField("name", "name");
+            int flow = RandomUtils.nextInt();
+            System.out.println(flow);
+            document.addField("flow", flow);
+            UpdateRequest request = new UpdateRequest();
+            request.add(document);
+            request.commit(lbHttpSolrClient, "core_1");
+        }
+    }
+
+}
