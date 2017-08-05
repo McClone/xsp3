d73faf86857c6c04211146bbf5225b82a802348d
mcclone <abc15606@163.com>
2017-08-05 11:10:37 +0800

    fix

--- src/main/java/org/mcclone/aop/SayHelloBeforeAdvice.java
+++ src/main/java/org/mcclone/aop/SayHelloBeforeAdvice.java
@@ -7,11 +7,11 @@ import org.springframework.aop.MethodBeforeAdvice;
 import java.lang.reflect.Method;
 
 /**
- * Created by mcclone on 17-8-4.
+ * @author mcclone
  */
 public class SayHelloBeforeAdvice implements MethodBeforeAdvice {
 
-    public static final Logger logger = LoggerFactory.getLogger(SayHelloBeforeAdvice.class);
+    private static final Logger logger = LoggerFactory.getLogger(SayHelloBeforeAdvice.class);
 
     @Override
     public void before(Method method, Object[] args, Object target) throws Throwable {
